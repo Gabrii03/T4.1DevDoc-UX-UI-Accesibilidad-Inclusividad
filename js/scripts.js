a74a3916208dd2fd045dd93d7b8b10ca3c07332e
Gabrii03 <114144425+Gabrii03@users.noreply.github.com>
2024-01-22 21:08:35 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('images').addEventListener('click', () => {
+    window.location.href = 'images.html';
+  });
+
+  document.getElementById('about').addEventListener('click', () => {
+    window.location.href = 'about.html';
+  });
+
+  document.getElementById('contact').addEventListener('click', () => {
+    window.location.href = 'contact.html';
+  });
